37e22917641477e554214907c177f7adcda00823
feng_xiao <xiaofeng@shuaiche.com>
2020-06-04 16:53:47 +0800

    feat: updated june 4

--- code.js
+++ code.js
@@ -1,49 +1,75 @@
-// 策略
-const strategies = {
-  checkName: (name) => {
-    if (name === 'jix') {
-      return true
-    }
-    return false
-  },
-  checkPhone: (phone) => {
-    if (typeof phone === 'number') {
-      return true
-    }
-    return false
-  }
+class PubSub {
+  constructor() {
+    this.handleBars = {}; // 保存监听事件
+  }
+  // 订阅
+  subscribe(eventName, handle) {
+    try {
+      if (!this.handleBars.hasOwnProperty(eventName)) {
+        this.handleBars[eventName] = [];
+      }
+      if (typeof handle == 'function') {
+        this.handleBars[eventName].push(handle);
+      } else {
+        throw new Error(`你需要给${eventName}事件添加回调方法`);
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+  // 发布
+  publish(eventName, ...arg) {
+    try {
+      if (this.handleBars.hasOwnProperty(eventName)) {
+        this.handleBars[eventName].map(item => {
+          item.apply(null, arg)
+        })
+      } else {
+        throw new Error(`${eventName}事件未被注册`);
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+  // 移除订阅
+  unSubscribe(eventName, handle) {
+    try {
+      if (this.handleBars.hasOwnProperty(eventName)) {
+        this.handleBars[eventName].map((item, index) => {
+          if (item === handle) {
+            this.handleBars[eventName].splice(index, 1)
+          }
+        })
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  }
 }
 
-let Validator = function() {
-  this.checkQueue = [] // 校验队列
+// 实例化
+const sub = new PubSub();
 
-  // 添加策略
-  this.add = (val, method) => { // val需要校验的值 method校验方法
-    this.checkQueue.push(()=> strategies[method](val))
-  }
-
-  // 校验
-  this.check = () => {
-    for(let i = 0; i < this.checkQueue.length; i++) {
-      let data = this.checkQueue[i]()
-      if(!data) {
-        return false
-      }
-    }
-    return true
-  }
+// 订阅的回调方法
+function func1(type) {
+  console.log('尼古拉斯赵四:', type);
 }
-
-let user1 = () => {
-  const validator = new Validator()
-  const data = {
-    name: 'jix',
-    phone: 666
-  }
-
-  validator.add(data.name, 'checkName')
-  validator.add(data.phone, 'checkPhone')
-  const result = validator.check()
-  return result
+function func2(type) {
+  console.log('王凯旋:', type);
 }
-console.log(user1())
+function func3(type) {
+  console.log('职业法师:', type);
+}
+// 订阅事件
+sub.subscribe('微信', func1)
+sub.subscribe('微信', func2)
+sub.subscribe('QQ', func3)
+  
+setTimeout(() => {
+  // 触发订阅事件
+  sub.publish('微信', '公众号推文')
+  sub.publish('QQ', '动态更新', '666')
+  // 移除订阅的ready事件func1回调
+  sub.unSubscribe('微信', func1);
+  console.log(sub.handleBars);
+}, 1000)
