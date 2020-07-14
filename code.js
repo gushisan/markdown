f927aa4ad32cf637b58f608e732026269b3b2345
feng_xiao <xiaofeng@shuaiche.com>
2020-07-14 21:00:46 +0800

    feat: 7.14正则todo

--- code.js
+++ code.js
@@ -1,51 +1,14 @@
-function filterParams(obj) { // 剔除对象的空属性
-  var _newObj = {};
-  for (var key in obj) {
-      if (obj.hasOwnProperty(key)) { // 判断对象中是否有这个属性
-          if (isEmpty(obj[key])) continue;
-          _newObj[key] = typeof obj[key] === 'object' ? (
-              obj[key] instanceof Array ? ArrayFilterParams(obj[key]) : filterParams(obj[key])
-          ): obj[key];
-          
-      }
-  }
-  return _newObj;
-}
-function ArrayFilterParams(arr) { // 剔除数组中的空值
-  var err = [];
-  const empty_arr = ['', undefined, null];
-  arr.forEach((item, index) => {
-      if (isEmpty(item)) return;
-      err.push(
-          typeof item === 'object' ? (
-              item instanceof Array ? ArrayFilterParams(item) :  filterParams(item)
-          ) : item
-      );
-  })
-  return err;
-}
-function isEmpty(obj) { // 为空情况
-  const empty_arr = ['', undefined, null];
-  return (empty_arr.indexOf(obj) > -1 || obj.toString().trim() === '');
-}
-let info = {
-  name: '111',
-  age: null,
-  sex: undefined,
-  aa: '',
-  infoo: [
-      1, 23,5434,null,22, {
-          nale: '',
-          ss: '111'
-      }
-  ],
-  cc: {
-      asas: [
-          1,2,23,333
-      ],
-      name: '',
-      asss: undefined,
-      aaa:null
-  }
-}
-console.log(filterParams(info));
+var str = '123456765464153513566'
+
+let a = str.replace(/(\d)(?=(\d{3})+$)/g,'$1,')
+console.log(a)
+// 分割数字每三个以一个逗号划分
+var test = "get-element-by-id";
+
+console.log(test.replace(/-\w/g, ($0) => {
+    return $0.slice(1).toUpperCase()
+}))
+
+var str3 = 'getElementById'
+
+console.log(str3.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase())
