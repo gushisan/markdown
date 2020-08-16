7b020e6d031bcfa3833bb7db9a67947ddf782919
feng xiao <xiaof@51xcb.cn>
2020-08-16 13:24:56 +0800

    feat: 8.16-函数式编程todo

--- code.js
+++ code.js
@@ -1,14 +1,8 @@
-// 带一个函数参数 和 该函数的部分参数
-const partial = (f, ...args) =>
-(...moreArgs) => f(...args, ...moreArgs)
+const compose = (f, g) => (x => f(g(x)));
 
-const add3 = (a, b, c) => a + b + c
-// 偏应用 `2` 和 `3` 到 `add3` 给你一个单参数的函数
-const fivePlus = partial(add3, 2, 3)
+var toUpperCase = word => word.toUpperCase();
 
-console.log(fivePlus(4))
+var split = x => (str => str.split(x));
 
-//bind实现
-const add1More = add3.bind(null, 2, 3)
-
-console.log(add1More(4))
+var f = compose(split(' '), toUpperCase);
+console.log(f("abcd aaa"));
